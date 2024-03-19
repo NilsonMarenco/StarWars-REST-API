dc17d1c33bac26d8dfcd6ef18150b2d5fad80637
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-03-19 11:47:45 +0000

    modelos

--- src/models.py
+++ src/models.py
@@ -8,6 +8,34 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(50))
+    population = db.Column(db.Integer)
+    terrain = db.Column(db.String(100))
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(20))
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    hair_color =db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, ForeignKey('character.id'))
+
+
     def __repr__(self):
         return '<User %r>' % self.username
 
